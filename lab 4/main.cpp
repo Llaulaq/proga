39b0b4980d52b1d332fd88fe712fc733e3fc855b
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-09-24 23:33:52 +0200

    Add: p1

--- lab 4/main.cpp
+++ lab 4/main.cpp
@@ -9,7 +9,21 @@ using namespace std;
  */
 
 int main() {
-    
+    int a, b, c;
+    cout << "Enter a, b, c: ";
+    cin >> a >> b >> c;
+
+    if (a < 1 || b < 1 || c < 1) {
+        cout << "Error" << endl;
+    } else {
+        if ((a % b == 0) && b > c) {
+            cout << a / b + c << endl;
+        } else if ((a % b == 0) && b < c) {
+            cout << a / b - c << endl;
+        } else {
+            cout << (a + b) * c << endl;
+        }
+    }
 
     return 0;
 }
