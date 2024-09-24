88a570ab59782fbd89ee3ad3f249dcd9bb333814
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-09-24 23:14:16 +0200

    Add: conversion

--- lab 3/main.cpp
+++ lab 3/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
 using namespace std;
-/* Var 1 */
+
+/* Var 1 
+Установить i-ый бит числа х (0<x<10^9) в 1. */
+
 int main() {
-    
+    unsigned int x;
+    int i;
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter i: ";
+    cin >> i;
+
+    cout << (x | (1 << i)) << endl;
+
     return 0;
 }
