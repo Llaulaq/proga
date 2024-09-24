5b434b999b80d050e1b0c69a0f92a11ac9d031c0
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-09-24 21:14:05 +0200

    Add: metod Gerona

--- lab 2/main.cpp
+++ lab 2/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
+/* Написать программу, которая на 
+вход получает 3 целых числа A, B, C – стороны треугольника. 
+Рассчитать площадь треугольника по формуле Герона.*/
 int main() {
-    cout << "Hi. This is a lab 2";
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a + b > c && a + c > b && b + c > a) {
+        double p = (a + b + c) * 1.0 / 2;
+        cout << "S = " << sqrt(p * (p - a)*(p - b) * (p - c));
+    } else {
+        cout << "Error";
+    }
+
     return 0;
 }
