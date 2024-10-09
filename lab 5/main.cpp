89e34d183b613f6262134b9979e2738e0e24b01e
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-10-09 10:09:54 +0200

    Add: p2

--- lab 5/main.cpp
+++ lab 5/main.cpp
@@ -41,5 +41,17 @@ int main() {
     }
 
 
+    int x, max_x = 0;
+    cin >> x;
+
+    while (x > 0) {
+        int digit = x % 10;
+        if (max_x < digit) 
+            max_x = digit;
+        x /= 10;
+    }
+
+    cout << "Max: " << max_x << endl;
+
     return 0;
 }
