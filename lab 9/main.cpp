4dc8b97a03df5a002d0f68df06fbb87aa5fd0e62
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-11-27 20:06:43 +0200

    Add: lab 9

---
+++ lab 9/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+//Var 1
+/* Дано целое число типа int, выведите на экран содержимое каждого из его байтов, 
+используя знания по указателям. Дайте обоснование полученному результату. 
+*/
+
+
+int main() {
+    int a{};
+    std::cin >> a;
+
+    unsigned char * p { (unsigned char *) &a };
+
+    for (int i = 0; i < sizeof(a); ++i) {
+        int b { *(p + i) };
+        std::cout << b << " ";
+    }
+
+    return 0;
+}
