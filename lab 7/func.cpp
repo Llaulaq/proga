f5182add119c95ee76e66c85c8e97e8c2772fd85
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-11-27 19:19:56 +0200

    Change: func sortMatris

--- lab 7/func.cpp
+++ lab 7/func.cpp
@@ -1,26 +1,38 @@
 #include <func.hpp>
 #include <iostream>
 
-const int MAX_SIZE = 100;
+const int MAX_SIZE { 100 };
 
 namespace rkh {
     
-    void Read(int matrix[MAX_SIZE][MAX_SIZE], int n, int m) {
+    bool read(int matrix[MAX_SIZE][MAX_SIZE], int& n, int& m) {
+        std::cout << "Enter the number of elements in the row of the matrix: ";
+        std::cin >> n;
+        std::cout << "Enter the number of elements in the matrix column: ";
+        std::cin >> m;
+
+        if (n < 1 || n > MAX_SIZE || 
+            m < 1 || m > MAX_SIZE) {
+            std::cerr << "Error: The number of items exceeds the allowed maximum." << '\n';
+            return false;
+        }
+
         std::cout << "Enter the sequence elements: ";
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 std::cin >> matrix[i][j];
             } 
         }
+        return true;
     }
 
 
-    void Write(int matrix[MAX_SIZE][MAX_SIZE], int n, int m) {
+    void write(int matrix[MAX_SIZE][MAX_SIZE], int n, int m) {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 std::cout << matrix[i][j] << " ";
             }
-            std::cout << std::endl;
+            std::cout << '\n';
         }
     }
 
@@ -65,20 +77,19 @@ namespace rkh {
         return false;
     }
 
-    void bubbleSort(int matrix[MAX_SIZE][MAX_SIZE], int n, int m) {
-        for (int i = 1; i < n - 1; ++i) {
-            for (int j = 0; j < n - i - 1; ++j) {
+    void sortMatrix(int matrix[MAX_SIZE][MAX_SIZE], int n, int m) {
+        for (int i = 0; i < n - 1; ++i) {
+            for (int k = i + 1; k < n; ++k) {
                 int sum1 = 0, sum2 = 0;
-                for (int t = 0; t < m; ++t) {
-                    sum1 += abs(matrix[i][t]);
-                    sum2 += abs(matrix[j][t]);      
+                for (int j = 0; j < m; ++j) {
+                    sum1 += abs(matrix[i][j]);
+                    sum2 += abs(matrix[k][j]);      
                 }
-
                 if (sum1 > sum2) {
-                    for (int t = 0; t < m; ++t) {
-                        std::swap(matrix[i][t], matrix[j][t]);
+                    for (int j = 0; j < m; ++j) {
+                        std::swap(matrix[i][j], matrix[k][j]);
                     }
-                } 
+                }
             }
         }
     }
--- lab 7/inc/func.hpp
+++ lab 7/inc/func.hpp
@@ -3,9 +3,9 @@
 namespace rkh {
     const int MAX_SIZE = 100;
 
-    void Read(int matrix[MAX_SIZE][MAX_SIZE], int n, int m);
-    void Write(int matrix[MAX_SIZE][MAX_SIZE], int n, int m);
+    bool read(int matrix[MAX_SIZE][MAX_SIZE], int& n, int& m);
+    void write(int matrix[MAX_SIZE][MAX_SIZE], int n, int m);
     bool identicalColumns(int matrix[MAX_SIZE][MAX_SIZE], int n, int m);
     bool isPrime(int matrix[MAX_SIZE][MAX_SIZE], int n, int m);
-    void bubbleSort(int matrix[MAX_SIZE][MAX_SIZE], int n, int m);
+    void sortMatrix(int matrix[MAX_SIZE][MAX_SIZE], int n, int m);
 }
