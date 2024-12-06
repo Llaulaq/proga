9cb182d1cdb3cf60cf7bdacaace7f5be61afc748
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-12-06 19:02:08 +0200

    Change: enumeration

--- dop 2/main.cpp
+++ dop 2/main.cpp
@@ -10,14 +10,29 @@ int main() {
     for (int i = 0; i <= 999'999; ++i) {
         int sum1 { 0 };
         int sum2 { 0 };
+        int num  { i };
+        int sh   { 0 }; 
 
-        for (int j = 100'000; j >= 1; j /= 10) {
+        /*for (int j = 100'000; j >= 1; j /= 10) {
             if (j >= 1000) {
                 sum1 += (i / j) % 10;
             }
             else {
                 sum2 += (i / j) % 10;
             }
+        }*/
+
+
+       while (num != 0) {
+            int x = num % 10;
+            sh++;
+            if (sh >= 4) 
+                sum1 += x;
+
+            else 
+                sum2 += x;
+            
+            num /= 10;
         }
 
         if (sum1 == sum2) {
