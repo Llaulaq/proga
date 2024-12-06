105bd5c95485faa41ca975d299ad26898a95d5a2
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-12-06 18:03:49 +0200

    Add: func read

--- dop 3/main.cpp
+++ dop 3/main.cpp
@@ -9,12 +9,34 @@
 const int MAX_SIZE { 10000 };
 
 
+bool read(int& cnt, int sequence[MAX_SIZE]) { 
+    std::cout << "Enter the number of items in the sequence: ";
+    std::cin >> cnt;
+
+    if (cnt < 1 || cnt > MAX_SIZE) {
+        std::cerr << "Error: The number of items exceeds the allowed maximum." << std::endl;
+        return false;
+    }
+
+    std::cout << "Enter the sequence elements: ";
+    for (int i = 0; i < cnt; ++i) {
+        std::cin >> sequence[i];
+        if (sequence[i] > MAX_SIZE) {
+            std::cerr << "Error: The element exceeds the allowed value.";
+            return true;
+        }
+    }
+}
+
+
 
 int main() {
     int cnt { 0 };
     int sequence[MAX_SIZE];
 
-    if (!read(cnt, sequence))
+    if (!read(cnt, sequence)) {
+        return -1;
+    }
 
 
     return 0;
