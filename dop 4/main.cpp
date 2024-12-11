7e7fd8bcdaec8d139ec3132258ac9fbf9e15a26e
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2024-12-11 23:34:40 +0200

    Add: mergeSort

--- dop 4/main.cpp
+++ dop 4/main.cpp
@@ -3,19 +3,19 @@
 const int MAX_SIZE = 10000; 
  
  
-void swapN(int i, int j, int mat[MAX_SIZE]) { 
-    int temp = mat[i]; 
-    mat[i] = mat[j]; 
-    mat[j] = temp; 
+void swapN(int i, int j, int mas[MAX_SIZE]) { 
+    int temp = mas[i]; 
+    mas[i] = mas[j]; 
+    mas[j] = temp; 
 } 
  
  
 //сортировка обменом 
-void swapSort(int n, int mat[MAX_SIZE]) { 
+void swapSort(int n, int mas[MAX_SIZE]) { 
     for (int i = 0; i < n - 1; ++i) { 
         for (int j = i + 1; j < n; ++j) { 
-            if (mat[i] > mat[j]) { 
-                swapN(i, j, mat); 
+            if (mas[i] > mas[j]) { 
+                swapN(i, j, mas); 
             } 
         } 
     } 
@@ -23,12 +23,12 @@ void swapSort(int n, int mat[MAX_SIZE]) {
  
  
 //сортировка пузырьком 
-void bubbleSort(int n, int mat[MAX_SIZE]) { 
+void bubbleSort(int n, int mas[MAX_SIZE]) { 
     for (int i = 0; i < n; ++i) { 
         bool flag = true; 
         for (int j = 0; j < n - i + 1; ++j) { 
-            if (mat[j] > mat[j + 1]) { 
-                swapN(j, j + 1, mat); 
+            if (mas[j] > mas[j + 1]) { 
+                swapN(j, j + 1, mas); 
                 bool flag = false; 
             } 
         } 
@@ -40,11 +40,11 @@ void bubbleSort(int n, int mat[MAX_SIZE]) {
  
  
 //сортировка Шелла 
-void shellSort(int n, int mat[MAX_SIZE]) { 
+void shellSort(int n, int mas[MAX_SIZE]) { 
     for (int step = n / 2; step > 0; step /= 2) { 
         for (int i = step; i < n; ++i) { 
-            for (int j = i - step; j >= 0 && mat[j] > mat[j + step]; j -= step) { 
-                swapN(j, j + 1, mat); 
+            for (int j = i - step; j >= 0 && mas[j] > mas[j + step]; j -= step) { 
+                swapN(j, j + 1, mas); 
             } 
  
         } 
@@ -52,38 +52,111 @@ void shellSort(int n, int mat[MAX_SIZE]) {
 } 
  
  
- 
 //быстрая сортировка 
-void quickSort(int n, int mat[]) { 
- 
+void quickSort(int start, int end, int mas[]) {
+    if (start > end) {
+        return;
+    }
+    int mid = mas[(start + end) / 2];
+    int i = start;
+    int j = end;
+
+    while (i <= j) {
+        while (mas[i] < mid) {
+        i++;
+        }
+        while (mas[j] > mid) {
+        j--;
+        }
+        if (i <= j) {
+            swapN(i, j, mas);
+            i++;
+            j--;
+        } 
+    }
+    quickSort(start, j, mas);
+    quickSort(i, end, mas);
 } 
  
  
- 
-//сортировка слиянием 
+//сортировка слиянием!!!!
+void merge(int start, int end, int mid, int mas[MAX_SIZE]) {
+    int mergedmas[MAX_SIZE];
+    int i, j, k;
+	i = start;
+	k = start;
+	j = mid + 1;
+	
+	while (i <= mid && j <= end) {
+		if (mas[i] < mas[j]) {
+			mergedmas[k] = mas[i];
+			k++;
+			i++;
+		}
+		else {
+			mergedmas[k] = mas[j];
+			k++;
+			j++;
+		}
+	}
+	
+	while (i <= mid) {
+		mergedmas[k] = mas[i];
+		k++;
+		i++;
+	}
+	
+	while (j <= end) {
+		mergedmas[k] = mas[j];
+		k++;
+		j++;
+	}
+	
+	for (i = start; i < k; i++) {
+		mas[i] = mergedmas[i];
+	}
+
+}
+
+
+void mergeSort(int start, int end, int mas[MAX_SIZE]) {
+    int mid{};
+	if (start < end) {
+		mid = (start + end) / 2;
+		mergeSort(start, mid, mas);
+		mergeSort(mid+1, end, mas);
+		merge(start, end, mid, mas);
+	}
+}
+
  
  
 //сортировка посчётом 
-void countingSort(int n, int mat[MAX_SIZE]) { 
-    int tmp_mat[MAX_SIZE]{}; 
+void countingSort(int n, int mas[MAX_SIZE]) { 
+    int tmp_mas[MAX_SIZE]{}; 
     for (int i = 0; i < n; ++i) { 
-        tmp_mat[mat[i]]++; 
+        tmp_mas[mas[i]]++; 
     } 
  
     int k = 0; 
     for (int i = 0; i < n; ++i) { 
-        while (tmp_mat[i] > 0) { 
-            mat[k] = i; 
+        while (tmp_mas[i] > 0) { 
+            mas[k] = i; 
             k++; 
-            tmp_mat[i]--; 
+            tmp_mas[i]--; 
         } 
     } 
  
 } 
  
  
-//сортировка кучей 
-void heapSort(int n, int mat[MAX_SIZE]) { 
+//сортировка кучей !!!
+
+
+
+
+
+void heapSort(int n, int mas[MAX_SIZE]) { 
  
 } 
  
@@ -91,12 +164,12 @@ void heapSort(int n, int mat[MAX_SIZE]) {
  
  
 int main() { 
-    int mat[5]{ 93, -86, 457, 5, -9 }; 
+    int mas[5]{ 93, -86, 457, 5, -9 }; 
  
-    shellSort(5, mat); 
+    quickSort(0, 4, mas); 
  
     for (int i = 0; i < 5; ++i) { 
-        std::cout << mat[i] << " "; 
+        std::cout << mas[i] << " "; 
     } 
  
     return 0; 
