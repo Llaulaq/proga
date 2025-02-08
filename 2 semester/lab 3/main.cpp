3d0a67d1c6f23323cab3898290c9df1e9599ea9a
Dasha Nikishina <dasha.nikishina.06@mail.ru>
2025-02-08 10:24:45 +0200

    Add: class Square

--- 2 semester/lab 3/main.cpp
+++ 2 semester/lab 3/main.cpp
@@ -4,7 +4,41 @@
 Квадрат, расчет площади, периметра и длины диагонали.
 */
 
+class Square {
+    public: 
+        int length;
+
+        Square(int d_lenght) {
+            length = d_lenght;
+        }
+
+        int getArea() {
+            return length * length;
+        }
+
+        int perimeter() {
+            return 4 * length;
+        }
+
+        int diagonal() {
+            return sqrt(2) * length;
+        }
+
+        void printInfo(bool detail) {
+            std::cout << "Length: " << length << '\n';
+        
+            if (detail) {
+                std::cout << "Area: " << getArea() << '\n';
+                std::cout << "Perimeter: " << perimeter() << '\n';
+                std::cout << "Diagonal: " << diagonal() << '\n';
+            }
+        }
+
+    private:
+};
 
 int main() {
-    
+    Square square1(10);
+
+    square1.printInfo(true);
 }
